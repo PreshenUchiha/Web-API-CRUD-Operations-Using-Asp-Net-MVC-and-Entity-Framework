b5b86c00c759cbc003b6d99b229f65e8cc846157
Preshen Reddy <PreshenR@hollywoodbets.net>
2023-05-09 09:44:28 +0200

    Add project files.

---
+++ MVC/Models/mvcEmployeeModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class mvcEmployeeModel
+    {
+        public int EmployeeID { get; set; }
+
+        [Required(ErrorMessage = "This Field is Required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "This Field is Required")]
+        public string Position { get; set; }
+        public Nullable<int> Age { get; set; }
+        public Nullable<int> Salary { get; set; }
+    }
+}
